592d62d1fb73bdab6b154d5e7fae1482b4f01afe
Bacher100 <bacher.smaali@iteam-univ.tn>
2026-03-30 18:56:10 +0200

    Create ReservationService class with reservation methods

---
+++ src/main/java/com/conference/service/ReservationService.java
@@ -0,0 +1,28 @@
+package com.conference.service;
+
+import java.util.List;
+
+public class ReservationService {
+
+    public void createReservation() {
+        // logic to create a reservation
+    }
+
+    public void updateReservation() {
+        // logic to update a reservation
+    }
+
+    public void cancelReservation() {
+        // logic to cancel a reservation
+    }
+
+    public List<String> getUpcomingReservations() {
+        // logic to get upcoming reservations
+        return null;
+    }
+
+    public List<String> getUserReservations() {
+        // logic to get reservations for a user
+        return null;
+    }
+}
